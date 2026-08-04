21c0842c54d9438a47fc0f337b1ca18a22731974
Qahtani1979 <admin@mvplab.sa>
2026-08-04 21:16:47 +0300

    fix(helm): renumber governance migrations, remove redundant Zeitwerk initializer files

--- modules/cityos-governance/config/initializers/zeitwerk.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-Rails.autoloaders.main.inflector.inflect(
-  "openproject_cityos_governance" => "OpenProject::CityOSGovernance"
-)
--- modules/cityos-strategy/config/initializers/zeitwerk.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Map hyphenated gem directory names to OpenProject module hierarchy.
-# openproject_cityos_strategy → OpenProject::CityOSStrategy
-Rails.autoloaders.main.inflector.inflect(
-  "openproject_cityos_strategy" => "OpenProject::CityOSStrategy"
-)
